a3e80cbe8581879a4615623c0b9904b1094a9f1a
Thai Binh <nguyenthaibinh0131@gmail.com>
2022-02-11 11:50:40 +0700

    fix: remove sign message

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@
 
 <script>
 import ConnectComponent from "./components/ConnectComponent.vue";
-import Web3 from "web3";
+// import Web3 from "web3";
 
 export default {
   name: "App",
@@ -46,12 +46,12 @@ export default {
       this.provider = provider;
       this.account = account;
       this.chainId = chainId;
-      const web3 = new Web3(provider);
-      const signature = await web3.eth.personal.sign(
-        `I am signing my message`,
-        account
-      );
-      this.signature = signature;
+      // const web3 = new Web3(provider);
+      // const signature = await web3.eth.personal.sign(
+      //   `I am signing my message`,
+      //   account
+      // );
+      // this.signature = signature;
     },
     disconnect() {
       this.provider && this.provider.close && this.provider.close();
