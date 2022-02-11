b8b7d88c6c4ddeb4dd60e0c4229fa192c9537e71
Thai Binh <nguyenthaibinh0131@gmail.com>
2022-02-11 15:13:05 +0700

    remove all localstorage

--- src/App.vue
+++ src/App.vue
@@ -69,9 +69,7 @@ export default {
       this.signature = null;
       // This localStorage key is set by @walletconnect/web3-provider
       // https://github.com/pancakeswap/pancake-frontend/blob/bdcb676700584f07b021679c6253dfed1db4d2ec/src/hooks/useAuth.ts#L65
-      if (localStorage.getItem("walletconnect")) {
-        localStorage.removeItem("walletconnect");
-      }
+      localStorage.clear();
     },
   },
 };
